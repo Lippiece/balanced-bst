9d84d77a7c867189d512af6bbcd87da131eb49ae
Lippiece <lippiece@proton.me>
2022-09-19 11:24:24 +0200

    add getDepth()

--- src/balanced-bst.js
+++ src/balanced-bst.js
@@ -43,7 +43,7 @@ const printTree
       tree.data } ${  printTree( tree.right ) }`;
 
   };
-const prettyPrint   = ( node, prefix = "", isLeft = true ) => {
+const prettyPrint = ( node, prefix = "", isLeft = true ) => {
 
   if ( node.right !== undefined ) {
 
@@ -58,9 +58,10 @@ const prettyPrint   = ( node, prefix = "", isLeft = true ) => {
   }
 
 };
-const immutableSort = array =>
-  [...array].sort( ( next, previous ) =>
-    next - previous );
+const immutableSort
+ = array =>
+   [...array].sort( ( next, previous ) =>
+     next - previous );
 const treeToArray
   = tree => {
 
@@ -107,25 +108,25 @@ const traverseLevelOrder
 
         const node = queue[ 0 ];
 
-        traverseLevelOrder( tree )( callback )(
+        return traverseLevelOrder( tree )( callback )(
           enqueue( queue )( callback )
         );
 
       };
 const enqueue
-= queue =>
-  callback => {
+  = queue =>
+    callback => {
 
-    const node = queue[ 0 ];
-    if ( node === undefined ) { return queue.slice( 1 ) }
-    callback( node );
-    return [
-      ...queue.slice( 1 ),
-      node.left,
-      node.right,
-    ];
+      const node = queue[ 0 ];
+      if ( node === undefined ) { return queue.slice( 1 ) }
+      callback( node );
+      return [
+        ...queue.slice( 1 ),
+        node.left,
+        node.right,
+      ];
 
-  };
+    };
 const traverseInOrder
   = tree =>
     callback => {
@@ -157,23 +158,46 @@ const traversePostOrder
 
     };
 const getHeight
-  = node => {
+  = tree => {
 
-    if ( !node ) { return -1 }
-
-    const left  = getHeight( node.left );
-    const right = getHeight( node.right );
-
-    return Math.max( left, right ) + 1;
+    if ( tree === undefined ) { return 0 }
+    return 1 + Math.max(
+      getHeight( tree.left ),
+      getHeight( tree.right )
+    );
 
   };
 const getDepth
-  = node => {};
-const array = generateArray( 5 );
-const tree  = makeTree( array );
-console.log( "initial", array );
+  = tree =>
+    node => {
 
+      if ( tree.data === node.data ) { return 0 }
+      return 1 + (
+        tree.data > node.data
+          ? getDepth( tree.left )( node )
+          : getDepth( tree.right )( node )
+      );
+
+    };
+const array       = generateArray( 5 );
+const initialTree = makeTree( array );
+const tree        = insert( initialTree )( [100, 200, 300] );
+console.log( "initial", array );
+console.log( "------------------" );
 prettyPrint( tree );
+console.log( "------------------" );
+console.log( "traverseLevelOrder" );
+traverseLevelOrder( tree )( node =>
+  console.log( node.data ) )();
+console.log( "------------------" );
+/*
+const heightToFind = find( newTree )( 2 );
+const depthToFind  = find( newTree )( newTree.data );
+console.log( `Height of ${ heightToFind.data } is`, getHeight( heightToFind ) );
+console.log( "------------------" );
+console.log( `Depth of ${ depthToFind.data } is`, getDepth( newTree )( depthToFind ) );
+console.log( "------------------" );
+console.log( "height", getHeight( find() ) );
 console.log( "------------------" );
 console.log( "inserted" );
 prettyPrint( insert( tree )( [66] ) );
@@ -181,17 +205,10 @@ console.log( "------------------" );
 console.log( "find 66" );
 console.log( find( insert( tree )( [66] ) )( 66 ) );
 console.log( "------------------" );
-/*
-console.log( "height", getHeight( tree ) );
-console.log( "------------------" );
 console.log( "removed" );
 prettyPrint( remove( tree )( 67 ) );
 console.log( "------------------" );
-console.log( "traverseLevelOrder" );
-traverseLevelOrder( tree )( node =>
-  console.log( node.data ) )();
-  console.log( "------------------" );
-  console.log( "Inorder" );
-  traverseInOrder( tree )( node =>
-    console.log( node.data ) );
-  */
+console.log( "Inorder" );
+traverseInOrder( tree )( node =>
+  console.log( node.data ) );
+*/
